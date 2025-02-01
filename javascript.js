441df3f8c5cfa2441735cf02b1043cccfb240533
Kasperrf <kasper_rf@hotmail.com>
2025-02-01 20:29:42 +0100

    Assignment finished, js runs 5 simulations of rock paper scissors

--- javascript.js
+++ javascript.js
@@ -3,13 +3,13 @@ let computerOutcome
 function computerChoice(){
     computerOutcome = Math.random()
     if (computerOutcome <= 0.3333){
-        computerOutcome = "Rock"
+        computerOutcome = "rock"
     }
     else if(computerOutcome >= 0.6666) {
-        computerOutcome = "Paper"
+        computerOutcome = "paper"
     }
     else{
-        computerOutcome = "Scissors"
+        computerOutcome = "scissors"
     }
     return computerOutcome
 }
@@ -17,9 +17,9 @@ function computerChoice(){
 let humanOutcome
 
 function humanChoice(){
-    humanOutcome = prompt("Choose between Rock Paper Scissor")
+    humanOutcome = prompt("Choose between Rock Paper Scissors")
     humanOutcome = humanOutcome.toLowerCase()
-    if (["rock", "paper", "scissor"].includes(humanOutcome)){
+    if (["rock", "paper", "scissors"].includes(humanOutcome)){
         return humanOutcome
     }
     else{
@@ -33,8 +33,40 @@ let humanScore = 0
 let computerScore = 0
 
 
-function playRound(computerChoice, humanChoice){
-    let comp = computerChoice()
-    let hum = humanChoice()
-    return comp, hum
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}. Its a draw!`)
+    }
+    else if(humanChoice == "rock" && computerChoice == "scissors") {
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}. You win!`)
+        humanScore ++
+    }
+    else if(humanChoice == "paper" && computerChoice == "rock") {
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}. You win!`)
+        humanScore ++
+    }
+    else if(humanChoice == "scissors" && computerChoice == "paper") {
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}. You win!`)
+        humanScore ++
+    }
+    else{
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}. You lose!`)
+        computerScore ++
+    }
 }
+  
+// const humanSelection = humanChoice();
+// const computerSelection = computerChoice();
+  
+// playRound(humanSelection, computerSelection);
+
+function playGame(){
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = humanChoice();
+        const computerSelection = computerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    console.log(`Your score is: ${humanScore} computer score is ${computerScore}`)
+}
+
+playGame()
